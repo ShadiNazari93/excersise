ea5ae9133cca474193b86c745f055c51de40c71f
Shadi Nazari <shadinazari.giramarketing@gmail.com>
2025-07-27 10:18:53 +0330

    debug

--- test_calculator.py
+++ test_calculator.py
@@ -7,19 +7,19 @@
 #########################################################
 # pip install pytest
 import pytest
-from calculator import add, subtract, multiply, divide
+from calculator import add, sub, mul, divide
 
 def test_add():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
 
 def test_subtract():
-    assert subtract(10, 5) == 5
-    assert subtract(0, 3) == -3
+    assert sub(10, 5) == 5
+    assert sub(0, 3) == -3
 
 def test_multiply():
-    assert multiply(2, 4) == 8
-    assert multiply(-2, 3) == -6
+    assert mul(2, 4) == 8
+    assert mul(-2, 3) == -6
 
 def test_divide():
     assert divide(10, 2) == 5
