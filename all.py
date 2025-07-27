ee30e598061f977cf3dfde1c81c007a53be98c8b
Shadi Nazari <shadinazari.giramarketing@gmail.com>
2025-07-27 09:34:07 +0330

    checking packages

---
+++ all.py
@@ -0,0 +1,8 @@
+import add
+print(add.add('3','5'))
+import subtract
+print(subtract.sub(3,5))
+import mult
+print(mult.mul(3,5))
+import dev
+print(dev.div(3,2))
---
+++ import.py
@@ -0,0 +1,5 @@
+from check import *
+print(div(2,3))
+print(mul(2,3))
+print(add(2,3))
+print(sub(2,3))
